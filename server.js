7dbae9f61fb8707e692058d27eee7aa9eb429642
User <user@example.com>
2026-07-27 13:26:46 +0800

    Fix wisteria position at envelope seal point

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const server = http.createServer((req, res) => {
     });
 });
 
-const PORT = 8094;
+const PORT = 8095;
 server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
 });
